440d8d4529a5c837d9ae52a1733293480ccbe717
Christian <christian911@sympatico.ca>
2020-02-28 01:17:59 -0500

    asm algorithm fixes

--- asm/ass3.asm
+++ asm/ass3.asm
@@ -1,48 +1,49 @@
-; Binary Tree Search Through Sorted Array Elements
-; Issues: ARM only addresses in 4 byte blocks so array length must satisfy 2^n-1
+; Process uses binary search tree algorithm to return the index of a specified input value in a sorted array
 
 ENTRY
 			
-			; using a specific number of indices (33) which allows for binary search tree algorithm to always land on a specific index when divided.
-array		DCD		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33
+array		DCD		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
 			
 PROC
 			
-_main
 _start
-			mov		r1,#8		; init search value
-			mov		r2,#128		; init array size in r2
-			mov		r3, #array	; load start address of array in r3
+			mov		r1,#17			; init search value in r1
+			mov		r2,#24			; init array size in r2
+			mov		r3, #array		; load start address of array in r3
+
+_main
 			mov		r7,#-1      	; while r7 is -1 the number is not found
 			cmp		r2,#0        	; ensure the array size is non-zero
 			blt		done        	; branch if less than to done
 			
-			mov		r5,#0       ; init min_index to 0
+			mov		r5,#0      		; init min_index to 0
+			sub		r2,r2,#1    	; init max_index
 			
 _while
-			cmp		r2,r5       ; check for elements left to search
+			cmp		r2,r5       	; check for elements left to search
 			blt		_done
 			
-			add		r4,r5,r2    ; determine midpoint index
-			lsr		r4,r4,#1    ; logical shift right by 1 index (int div/2)
+			add		r4,r5,r2    	; determine midpoint index
+			lsl		r4,r4,#1    	; logical shift right by 1 index (int r4/2) then conversion off offset from bit to byte increments lsl << 2 (int r4*4)
+			ldr		r8,[r3,r4]  	; get midpoint index value
 			
-			ldr		r8,[r3,r4]  	; get midpoint index
-			cmp		r8,r1       ; test midpoint index
-			blt		_adjust_min  ; less than branch (adjust max index)
-			bgt		_adjust_max  ; greater than branch (adjust min index)
+			cmp		r8,r1       	; test midpoint index
+			blt		_adjust_min  	; less than branch (adjust max index)
+			bgt		_adjust_max  	; greater than branch (adjust min index)
 			
-			mov		r7,r4       ; else we have the exact number in the array so we have found the index
+			mov		r7,r4       	; else we have the exact number in the array so we have found the index
 			b		_done
 			
 _adjust_min
-			add		r5,r4,#0
+			lsr		r4,r4,#2		; convert word (byte) index to integer index for min increment
+			add		r5,r4,#1
 			b		_while
 			
 _adjust_max
-			sub		r2,r4,#0
+			lsr		r4,r4,#2		; convert word (byte) index to integer index for max decrement
+			sub		r2,r4,#1
 			b		_while
 			
 _done
 ENDP
-			
 			END
