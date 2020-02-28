b51d48323a097be374e922955240012ee8e3dcf0
Christian <christian911@sympatico.ca>
2020-02-28 01:24:16 -0500

    lsr and lsl not inverse operations

--- asm/ass3.asm
+++ asm/ass3.asm
@@ -1,5 +1,5 @@
-; Process uses binary search tree algorithm to return the index of a specified input value in a sorted array
-
+			;		Process uses binary search tree algorithm to return the index of a specified input value in a sorted array
+			
 ENTRY
 			
 array		DCD		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
@@ -10,7 +10,7 @@ _start
 			mov		r1,#17			; init search value in r1
 			mov		r2,#24			; init array size in r2
 			mov		r3, #array		; load start address of array in r3
-
+			
 _main
 			mov		r7,#-1      	; while r7 is -1 the number is not found
 			cmp		r2,#0        	; ensure the array size is non-zero
@@ -24,7 +24,8 @@ _while
 			blt		_done
 			
 			add		r4,r5,r2    	; determine midpoint index
-			lsl		r4,r4,#1    	; logical shift right by 1 index (int r4/2) then conversion off offset from bit to byte increments lsl << 2 (int r4*4)
+			lsr		r4,r4,#1    	; logical shift right by 1 index (int r4/2)
+			lsl		r4,r4,#2		; then offset from bit to byte increments lsl << 2 (int r4*4)
 			ldr		r8,[r3,r4]  	; get midpoint index value
 			
 			cmp		r8,r1       	; test midpoint index
